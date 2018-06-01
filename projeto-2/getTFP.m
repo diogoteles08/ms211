810091b62c6806010056b63b95dc02a91033f887
Luciano Gigantelli Zago <ra182835@students.ic.unicamp.br>
2018-06-01 17:32:19 -0300

    Fix getTFP (inverted)

--- projeto-2/getTFP.m
+++ projeto-2/getTFP.m
@@ -6,15 +6,14 @@ function tfp = getTFP(L, s, ytr)
 
   for i = 1:m
     if (L < s(i)) % detectado spam
-      if (ytr(i) == 1) % mas nao eh spam
+      if (ytr(i) == -1) % mas nao eh spam
         wspam++;
       end
     end
-    if (ytr(i) == -1) % se eh spam
+    if (ytr(i) == 1) % se eh spam
       spam++;
     end
   end
 
   tfp = wspam/spam;
-
 end
